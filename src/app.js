0d63a4de86df6457d9f1ece98524b107e4504cb7
Alejandra36a <alejandra36a@gmail.com>
2024-05-30 17:40:55 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+  console.log("Hello Rigo from the console");
+};
+let generateExcuse = () => {
+  let who = ["The dog", "The car", "A girl", "My father", "I"];
+  let action = ["ate ", "broke down", "won the"];
+  let what = ["my homework", "the lottery", "my phone"];
+  let when = ["yesterday", "on sunday", "today ", "the last month"];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
